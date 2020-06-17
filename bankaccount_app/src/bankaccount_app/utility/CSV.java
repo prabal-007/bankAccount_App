dc856295bd79721e7161d6774f3ddde38e3fbc16
Starkk <42775517+prabal-007@users.noreply.github.com>
2020-06-17 14:57:45 +0530

    Add files via upload

---
+++ bankaccount_app/src/bankaccount_app/utility/CSV.java
@@ -0,0 +1,35 @@
+package utility;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class CSV {
+	
+	public static List<String[]> read(String file) {
+		List<String[]> data = new LinkedList<String[]>();
+		String dataRow;
+			
+		try {
+//			@SuppressWarnings("resource")
+			BufferedReader br = new BufferedReader(new FileReader(file));	
+			while ((dataRow = br.readLine()) != null); {
+//				@SuppressWarnings("null")
+				String[] dataRecords = dataRow.split(",");
+				data.add(dataRecords);
+				}
+					
+			} catch (FileNotFoundException e) {
+				System.out.println("can not read file");
+				e.printStackTrace();
+			}catch (IOException e) {
+				System.out.println("can not read file");
+				e.printStackTrace();
+			}
+		return data;
+			
+		}
+	}
