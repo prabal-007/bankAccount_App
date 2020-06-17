aeb63ae6efc9ea662f77a7b7d588dce7e29772f5
Starkk <42775517+prabal-007@users.noreply.github.com>
2020-06-17 14:37:47 +0530

    Update BankAccount_app.java

--- bankaccount_app/src/bankaccount_app/BankAccount_app.java
+++ bankaccount_app/src/bankaccount_app/BankAccount_app.java
@@ -1,27 +1,59 @@
 package bankaccount_app;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class BankAccount_app {
 
-	public static void main(String[] args) {
-		
-		checking check1 = new checking("Prabal Gupta", "1234567890", 2000);
+	public static void main(String[] args) {		
+	/*
+		checking check1 = new checking("Tony Stark", "1234567890", 2000);
 				
-		saveings savcc1 = new saveings("tony Stark", "1234567891", 1500);
+		saveings savcc1 = new saveings("Steve Rogers", "1234567891", 1500);
 		
-		savcc1.compound();
+//		savcc1.compound();
 		
 		savcc1.showInfo();
 		System.out.println("**********************************");
 		check1.showInfo();
 		
-//		savcc1.deposit(2000);
-//		savcc1.withdrawl(200);
-//		check1.transfer("tony", 500);
+		savcc1.deposit(2000);
+		savcc1.withdrawl(200);
+		check1.transfer("tony", 500);
+	*/
 		
+		List<Account> accounts = new LinkedList<Account>();
 	
 		
 		// Reads CSV file then creates new account based on that data
-
+		
+		String file = "//root/Documents//java_projects//New_bank_Account_app//docs//original.csv"; 
+		
+		
+			
+			List<String[]> newAccountHolder = utility.CSV.read(file);
+			for (String[] accountHolder : newAccountHolder) {
+				System.out.println("NEW ACCOUNT ");
+				String name = accountHolder[0];
+				String sSN = accountHolder[1];
+				String accountType = accountHolder[2];
+				double initDeposit = Double.parseDouble(accountHolder[3]);
+				System.out.println(name+" "+sSN+" "+accountType+" "+initDeposit);
+				if (accountType.equals("Savings")) {
+					accounts.add(new saveings(name, sSN, initDeposit));
+				}
+				else if (accountType.equals("Checking")) {
+					accounts.add(new checking(name, sSN, initDeposit));
+				}
+				else {
+					System.out.println("ERROR IN READING ACCOUNT TYPE!");
+				}
+			}
+		
+		for (Account acc : accounts) {
+			System.out.println("////////////////////////");
+			acc.showInfo();
+		}
 	}
 
 }
